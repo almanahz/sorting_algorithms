9dd2aa88be58e0e3dbb3c4d7ef30b18a105dc8d6
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-24 03:25:13 -0700

    project one

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,32 @@
 #include "sort.h"
-/*  desc: sort an array in ascending order using bubble sort
-    algorithm
-    return: -
-*/
-
+/**
+ * bubble_sort - sort an array in ascending order using bubble sort
+ *
+ * @array: the array to be sorted
+ * @size: the size of array
+ * return: -
+ */
 void bubble_sort(int *array, size_t size)
 {
-    int i, j, temp, flag;
-    flag = 0;
-    // i rep number of passes
-    // j rep number of element to compare in each passes
-    // temp is to swap the variables
-    // flag is to produce an optimized bubble if the rest of array
-    // is sorted
-    for (i = 0; i < size + 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = temp;
-                flag = 1;
-            }
-        }
-        if(flag == 0)
-        break;
-    }
+	int i, j, temp, a, flag;
+
+	for (i = 0; i < size + 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+		if (array[j] > array[j + 1])
+		{
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			print_array(array, size);
+			flag = 1;
+		}
+	}
+	if (flag == 0)
+	{
+	break;
+	}
+	}
 }
