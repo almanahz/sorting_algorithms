7d92ffe9fa51f1c9102170a81f8e119c3f732a56
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-24 05:45:05 -0700

    updated project 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,13 +8,14 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, swap;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
+		swap = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,7 +24,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+				swap = 1
 			}
 		}
+		if (swap == 0)
+			break;
 	}
 }
